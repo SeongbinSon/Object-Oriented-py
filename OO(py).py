80d0a545905c85dcc75bdb4b9096b3d196cf9b22
Seongbin Son <152048071+SeongbinSon@users.noreply.github.com>
2024-04-06 15:51:32 +0900

    Add files via upload
    코드추가본

--- OO(py).py
+++ OO(py).py
@@ -223,3 +223,60 @@ sally.push_pet("이구아나")
 print(sally.pet)
 
 #--------------------------------------------------------------------------#
+
+class Members:
+   total = 0
+
+   def __init__(self, name, phone):
+      self.name = name
+      self.phone = phone
+      Members.total = Members.total + 1
+
+   def show_info(self):
+      print("이름 : %s, 전화번호 : %s" % (self.name, self.phone))
+
+m1 = Members("홍성지", "010-3359-3763")
+m2 = Members("강동욱", "010-1019-4767")
+m3 = Members("신진서", "010-9018-0298")
+
+m1.show_info()
+m2.show_info()
+m3.show_info()
+
+print("총 회원 수 :", Members.total)
+
+#--------------------------------------------------------------------------#
+
+class Person:
+   def __init__(self, name):
+      self.name = name
+
+   def show_name(self):
+      print(self.name)
+
+class Student(Person):
+   pass
+
+x = Student("홍길동")
+x.show_name()
+
+#--------------------------------------------------------------------------#
+
+class Person:
+   def __init__(self, name):
+      self.name = name
+   def show_name(self):
+      print(self.name)
+   def show_age(self):
+      print(self.age)
+
+class Student(Person):
+   def __init__(self, name, age):
+      super().__init__(name)
+      self.age = age
+
+x = Student("홍길동", 20)
+x.show_name()
+x.show_age()
+
+#--------------------------------------------------------------------------#
